5873425c1787b774ec5b82ef2c753749a07cad77
sihoney <hwangheewon@naver.com>
2022-01-15 22:59:20 +0900

    linkedList deque

--- src/Deque/LinkedListDeque.java
+++ src/Deque/LinkedListDeque.java
@@ -6,6 +6,8 @@
 
 package Deque;
 
+import java.util.NoSuchElementException;
+
 import interface_form.Queue;
 
 public class LinkedListDeque<E> implements Queue<E> {
@@ -108,13 +110,197 @@ public class LinkedListDeque<E> implements Queue<E> {
 			return null;
 		}
 		
+		E element = head.data;
 		
+		Node<E> nextNode = head.next;
+		
+		head.data = null;
+		head.next = null;
+		
+		if(nextNode != null) {
+			nextNode.prev = null;
+		}
+		
+		head = null;
+		head = nextNode;
+		size--;
+		
+		if(size == 0) {
+			tail = null;
+		}
+		
+		return element;
 	}
 
+	public E remove() {
+		return removeFirst();
+	}
+	
+	public E removeFirst() {
+		E element = poll();
+		
+		if(element == null) {
+			throw new NoSuchElementException();
+		}
+		
+		return element;
+	}
+	
+	public E pollLast() {
+		if(size == 0) {
+			return null;
+		}
+		
+		E element = tail.data;
+		
+		Node<E> prevNode = tail.prev;
+		
+		tail.data = null;
+		tail.prev = null;
+		
+		if(prevNode != null) {
+			prevNode.next = null;
+		}
+		
+		tail = null;
+		tail = prevNode;
+		size--;
+		
+		if(size == 0) {
+			head = null;
+		}
+		
+		return element;
+	}
+	
+	public E removeLast() {
+		E element = pollLast();
+		
+		if(element == null) {
+			throw new NoSuchElementException();
+		}
+		
+		return element;
+	}
+	
+	/*
+	 * [peek 게열 메소드 구현]
+	 */
+	
 	@Override
 	public E peek() {
-		// TODO Auto-generated method stub
-		return null;
+		return peekFirst();
+	}
+	
+	public E peekFirst() {
+		// 요소가 없을 경우 null 반환
+		if(size == 0) {
+			return null;
+		}
+		
+		return head.data;
+	}
+	
+	public E peekLast() {
+		if(size == 0) {
+			return null;
+		}
+		
+		return tail.data;
 	}
 
+	public E element() {
+		return getFirst();
+	}
+	
+	public E getFirst() {
+		E item = peek();
+		
+		if(item == null) {
+			throw new NoSuchElementException();
+		}
+		
+		return item;
+	}
+	
+	public E getLast() {
+		E item = peekLast();
+		
+		if(item == null) {
+			throw new NoSuchElementException();
+		}
+		return item;
+	}
+	
+	/*
+	 * [size, isEmpty, contains, clear 메소드 구현]
+	 */
+	
+	public int size () {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public boolean contains(Object value) {
+		
+		for(Node<E> x = head; x != null; x = x.next) {
+			if(value.equals(x.data)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public void clear() {
+		for(Node<E> x = head; x != null;) {
+			Node<E> next = x.next;
+			
+			x.data = null;
+			x.next = null;
+			x.prev = null;
+			
+			x = next;
+		}
+		
+		size = 0;
+		head = tail = null;
+	}
+	
+	/*
+	 * [toArray, clone, sort 메소드 구현]
+	 */
+	
+	public Object[] toArray() {
+		Object[] array = new Object[size];
+		int idx = 0;
+		for(Node<E> x = head; x != null; x = x.next) {
+			array[idx++] = (E) x.data;
+		}
+		
+		return array;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T> T[] toArray(T[] a) {
+		if(a.length < size) {
+			a = (T[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);
+		}
+		
+		int i = 0;
+		Object[] result = a;
+		
+		for(Node<E> x = head; x != null; x = x.next) {
+			result[i++] = x.data;
+		}
+		
+		return a;
+	}
+	
+	public Object clone() {
+		
+	}
 }
